6fc371939e4f023ed3d87c88f84629762dc01bce
prashantkumar1113 <prashantkumar1113@gmail.com>
2021-11-18 01:21:57 -0600

    Added the mock api and first function component

---
+++ src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="container">
+      <div className="jumbotron">
+        <h1>Hello, World!</h1>
+        <p>React, Flux, and React Router by Prashant Kumar</p>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
